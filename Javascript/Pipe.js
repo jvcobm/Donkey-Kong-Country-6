1b9848e27cbbf5c2fc27d47d6e8e8a066f085cab
jvcobm <41878143+jvcobm@users.noreply.github.com>
2018-10-15 12:42:49 +1300

    Add files via upload

---
+++ Javascript/Pipe.js
@@ -0,0 +1,55 @@
+class Pipe {
+    constructor(x, y, bY, w, h, c, xSpeed, gap) {
+        this.x = x;
+        this.y = y;
+        this.bY = bY;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.xSpeed = xSpeed;
+        this.gap = gap;
+    }
+    
+    drawPipe() {
+        canvasContext.fillStyle = this.c;
+        canvasContext.fillRect(this.x, this.y, this.w, this.h);
+        canvasContext.fillRect(this.x, this.bY, this.w, canvas.height - this.gap - this.y);
+        
+        canvasContext.drawImage(imgs.pipe, 0, 0, 201, 204, this.x - 6, this.y, this.w + 12, this.h + 6);
+        
+        canvasContext.drawImage(imgs.pipeb, 0, 0, 201, 204, this.x - 12, this.bY - 12, this.w + 24  , canvas.height - this.gap - this.y);
+    }
+
+    pipeMove() {
+        this.x += this.xSpeed;
+
+        if (this.x < 0 - this.w) {
+            pipes.pop();
+            setUpPipe = true;
+            for (i = 0; i < pipeTotal; i++) {
+                makePipe();
+            }
+        }
+    }
+
+    pipeHit() {
+        if (ppY < this.h && canvas.width / 3 + ppW > this.x && canvas.width / 3 < this.x + this.w || ppY + ppW > this.bY && canvas.width / 3 + ppW > this.x && canvas.width / 3 < this.x + this.w) {
+            gameOver = true;
+            this.c = 'red';
+        } else {
+            this.c = 'green';
+        }
+    }
+    scoreAdd() {
+        if (this.x + this.w > canvas.width / 3 && this.w + this.x < canvas.width / 3 + 3) {
+            score = score + 1;
+            console.log(score);
+        }
+    }
+
+    pipeHeight() {
+        this.h = Math.floor((Math.random() * 200) + 150);
+        this.bY = this.h + this.gap;
+    }
+
+}
---
+++ Javascript/player.js
@@ -0,0 +1,49 @@
+class Player {
+    constructor(x, y, w, h, c, ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.ySpeed = ySpeed;
+
+    }
+
+    drawPlayer() { // Draw the player (similar to colorRect)
+//        canvasContext.fillStyle = this.c;
+//        canvasContext.fillRect(this.x, this.y, this.w, this.h);
+        canvasContext.drawImage(imgs.player, 0, 60, 200, 200, this.x, this.y , this.w + 5, this.h + 15);
+    }
+
+    pFall() { // makes the player fall
+        this.ySpeed += gravity;
+        if (this.y > canvas.height) {
+            gameOver = true;
+        }
+    }
+
+    pMove() { // makes the player move (gravity and jumping)
+
+        this.y += this.ySpeed;
+    }
+
+    pJump() { // makes the player jump
+        if (jump && this.y > 0 + this.h) {
+            this.ySpeed = jHeight;
+            jump = false;
+        }
+    }
+
+    updateYpos() {
+        ppY = this.y;
+        ppW = this.w;
+        ppH = this.h;
+    }
+    
+    deathResetP(){
+        if(gameOver){
+            this.ySpeed = 0;
+            this.y = 0;
+        }
+    }
+}
