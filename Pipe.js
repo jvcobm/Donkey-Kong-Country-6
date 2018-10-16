06f75eef059ef1d8cef2828b9fc04b0b2356fad3
jvcobm <41878143+jvcobm@users.noreply.github.com>
2018-10-17 11:17:06 +1300

    Add files via upload

--- Pipe.js
+++ Pipe.js
@@ -1,5 +1,5 @@
-class Pipe{
-    constructor(x,y,bY,w,h,c,xSpeed,gap){
+class Pipe {
+    constructor(x, y, bY, w, h, c, xSpeed, gap) {
         this.x = x;
         this.y = y;
         this.bY = bY;
@@ -9,28 +9,48 @@ class Pipe{
         this.xSpeed = xSpeed;
         this.gap = gap;
     }
-
-    drawPipe(){
-        canvasContext.fillStyle = this.c;
-        canvasContext.fillRect(this.x,this.y,this.w,this.h);
-        canvasContext.fillRect(this.x,this.bY,this.w,canvas.height - this.gap - this.y);
-    }
     
-    pipeMove(){
+    drawPipe() { // Draws pipe  using images loaded in DKC4.html
+        canvasContext.fillStyle = this.c;
+        canvasContext.fillRect(this.x, this.y, this.w, this.h);
+        canvasContext.fillRect(this.x, this.bY, this.w, canvas.height - this.gap - this.y);
+        
+        canvasContext.drawImage(imgs.pipe, 0, 0, 201, 204, this.x - 6, this.y, this.w + 12, this.h + 6);
+        
+        canvasContext.drawImage(imgs.pipeb, 0, 0, 201, 204, this.x - 12, this.bY - 12, this.w + 24  , canvas.height - this.gap - this.y);
+    }
+
+    pipeMove() { // moves the pipe across the screen
         this.x += this.xSpeed;
-        if(this.x < 0 - this.w){
-            this.x = canvas.width;
-            this.h = Math.floor((Math.random() * 200) + 150);
-            this.bY = this.h + this.gap;
+
+        if (this.x < 0 - this.w) {
+            pipes.pop();
+            setUpPipe = true;
+            for (i = 0; i < pipeTotal; i++) {
+                makePipe();
+            }
         }
     }
 
-    pipeHit(){
-        if (ppY < this.h && canvas.width / 3 + ppW > this.x && canvas.width / 3 < this.x + this.w|| ppY + ppW> this.bY && canvas.width / 3 + ppW > this.x && canvas.width / 3 < this.x + this.w){
-                gameOver = true;
-                this.c = 'red';
-        } else{
+    pipeHit() { // detects when the player hits the pipe
+        if (ppY < this.h && canvas.width / 3 + ppW > this.x && canvas.width / 3 < this.x + this.w || ppY + ppW > this.bY && canvas.width / 3 + ppW > this.x && canvas.width / 3 < this.x + this.w) {
+            gameOver = true;
+            this.c = 'red';
+        } else {
             this.c = 'green';
         }
     }
+    
+    scoreAdd() { // increases the score when the player passes throught the pipe
+        if (this.x + this.w > canvas.width / 3 && this.w + this.x < canvas.width / 3 + 3) {
+            score = score + 1;
+            console.log(score);
+        }
+    }
+
+    pipeHeight() { // randomises the height of the top side of the pipe
+        this.h = Math.floor((Math.random() * 200) + 150);
+        this.bY = this.h + this.gap;
+    }
+
 }
--- player.js
+++ player.js
@@ -1,40 +1,47 @@
-class Player{
-    constructor(x,y,w,h,c,ySpeed){
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
-            this.c = c;
-            this.ySpeed = ySpeed;
-                
+class Player {
+    constructor(x, y, w, h, c, ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.c = c;
+        this.ySpeed = ySpeed;
+
+    }
+
+    drawPlayer() { // Draw the player (similar to colorRect)
+        canvasContext.drawImage(imgs.player, 0, 60, 200, 200, this.x, this.y , this.w + 5, this.h + 15);
+    }
+
+    pFall() { // makes the player fall
+        this.ySpeed += gravity;
+        if (this.y > canvas.height) {
+            gameOver = true;
         }
-    
-     drawPlayer(){ // Draw the player (similar to colorRect)
-            canvasContext.fillStyle = this.c;
-            canvasContext.fillRect(this.x,this.y,this.w,this.h);
+    }
+
+    pMove() { // makes the player move (gravity and jumping)
+
+        this.y += this.ySpeed;
+    }
+
+    pJump() { // makes the player jump
+        if (jump && this.y > 0 + this.h) {
+            this.ySpeed = jHeight;
+            jump = false;
         }
-            
-     pFall(){ // makes the player fall
-            this.ySpeed += gravity;
-            if(this.y > canvas.height){
-                gameOver = true;
-            }
-        }
-           
-     pMove(){ // makes the player move (gravity and jumping)
-            this.y += this.ySpeed;
-        }
-           
-     pJump(){ // makes the player jump
-            if(jump && this.y > 0 + this.h){
-                this.ySpeed = jHeight;
-                jump = false;
-            }
-        }
-    
-    updateYpos(){
+    }
+
+    updateYpos() { // Updates is position to be used in pipe.js
         ppY = this.y;
         ppW = this.w;
         ppH = this.h;
+    }
+    
+    deathResetP(){ // Resets the player after death
+        if(gameOver){
+            this.ySpeed = 0;
+            this.y = 0;
+        }
     }
 }
